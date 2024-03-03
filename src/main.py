619544f33f2057614c4a03bbe84bc88732a14383
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-03-03 19:41:00 +0300

    Add handler /start & /help & echo

--- src/main.py
+++ src/main.py
@@ -4,22 +4,45 @@ import os
 import pymongo
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
 from dotenv import load_dotenv
-from config import Config
+
 # import handlers
-from logger import logger
 
 load_dotenv()
 
-API_TOKEN = os.getenv('API_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-bot = Bot(token=API_TOKEN)
+# bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def handle_start(message: types.Message):
+    await message.answer(text=f"Приветики, {message.from_user.full_name}!")
+
+
+@dp.message(Command("help"))
+async def handle_help(message: types.Message):
+    text = "Я бот отвечающий на агрегацию db Mongodb.\nМожно выбрать 3 запроса!"
+    await message.answer(text=text)
+
+
+@dp.message()
+async def echo_message(message: types.Message):
+    await message.answer(
+        text="Такого обработчика нет",
+    )
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text="Что-то такое...")
 
 
 async def main() -> None:
     """Основная логика для запуска."""
     logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=BOT_TOKEN)
     await dp.start_polling(bot)
 
 
